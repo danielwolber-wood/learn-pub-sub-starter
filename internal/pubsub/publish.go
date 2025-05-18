c5e0f15b66411186983cf8659ec2a370f9e8dfc2
danielwolber-wood <daniel@wolber-wood.com>
2025-05-18 11:30:06 -0500

    Final Commit

---
+++ internal/pubsub/publish.go
@@ -0,0 +1,35 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	dat, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        dat,
+	})
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var encoded bytes.Buffer
+	encoder := gob.NewEncoder(&encoded)
+	err := encoder.Encode(val)
+	if err != nil {
+		return err
+	}
+	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/gob",
+		Body:        encoded.Bytes(),
+	})
+
+}
